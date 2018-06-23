cd79523e0696dea6f60d4f5e8b110460a3ea8742
SeHyun Park <crepepepes@gmail.com>
2018-06-23 09:31:19 +0900

    Update intsetbst.cpp

--- intsetbst.cpp
+++ intsetbst.cpp
@@ -1,37 +1,100 @@
 #include "intset.hpp"
-IntSetBST::IntSetBST(unsigned int max_element, int max_val) : IntSet(max_elements, max_val)
+
+IntSetBST::IntSetBST(unsigned int max_elements, int max_val) : IntSet(max_elements, max_val)
 {
-    typedef struct Node
-    {
-        int data;
-        struct Node*left;
-        struct Node*right;
-    } Node;
-    
 }
+
 IntSetBST::~IntSetBST()
 {
-    delete[] BSTNode;
-}
-void IntSetBST::insert(Node * parent, int element)
-{
-    if(parent -> data < data)
+    if (this->set_size == 0)
     {
-        if(parent-> left == nullptr)
-            parent->left = new Node({ data, nullptr, nullptr});
-        else
-            insert(parent->left, data);
+        return;
+    }
+
+    for (unsigned int i = 0; i < node_vector.size(); i++)
+    {
+        delete node_vector[i];
+    }
+
+    delete this->root_node;
+}
+
+void IntSetBST::insert(int element)
+{
+    if (element > this->max_val || this->set_size >= this->max_elements)
+    {
+        return;
+    }
+
+    if (this->root_node == nullptr)
+    {
+        this->root_node = new struct bstNode;
+        setBstNode(element, this->root_node);
+        this->set_size++;
     }
     else
     {
-        if(parent -> right == nullptr)
-            parent->right = new Node({ data, nullptr, nullptr});
-        
-        else
-            insert(parent->right, data);
+        struct bstNode *iter = this->root_node;
+        while (iter)
+        {
+            if (element > iter->val)
+            {
+                if (iter->right == nullptr)
+                {
+                    iter->right = new struct bstNode;
+                    iter = iter->right;
+                    break;
+                }
+                else
+                {
+                    iter = iter->right;
+                    continue;
+                }
+            }
+            else if (element < iter->val)
+            {
+                if (iter->left == nullptr)
+                {
+                    iter->left = new struct bstNode;
+                    iter = iter->left;
+                    break;
+                }
+                else
+                {
+                    iter = iter->left;
+                    continue;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        setBstNode(element, iter);
+        node_vector.push_back(iter);
+        this->set_size++;
     }
 }
+
 const void IntSetBST::report(int *v)
 {
-    
+    if (this->set_size == 0)
+    {
+        return;
+    }
+
+    vector<int> result;
+    result.push_back(root_node->val);
+    for (unsigned int i = 0; i < node_vector.size(); i++)
+    {
+        result.push_back(node_vector[i]->val);
+    }
+
+    sort(result.begin(), result.end());
+
+    for (unsigned int i = 0; i < node_vector.size(); i++)
+    {
+        v[i] = result[i];
+    }
 }
